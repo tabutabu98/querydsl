0fe6a94f5fcf0601ce5cb84514fe154d45cb3b49
syc <syc@partdb.com>
2022-03-28 23:40:03 +0900

    Add: 순수 JPA 리포지토리와 Querydsl @tabutabu98

---
+++ src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,62 @@
+package study.querydsl.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.domain.Member;
+
+import javax.persistence.EntityManager;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void basicTest() throws Exception {
+        // given
+        Member member = new Member("member1", 10);
+        memberJpaRepository.save(member);
+
+        // when
+        Member findMember = memberJpaRepository.findById(member.getId()).get();
+
+        // then
+        assertThat(findMember).isEqualTo(member);
+
+        List<Member> result1 = memberJpaRepository.findAll();
+        assertThat(result1).containsExactly(member);
+
+        List<Member> result2 = memberJpaRepository.findByUsername("member1");
+        assertThat(result2).containsExactly(member);
+    }
+
+    @Test
+    public void basicQuerydslTest() throws Exception {
+        // given
+        Member member = new Member("member1", 10);
+        memberJpaRepository.save(member);
+
+        // when
+        Member findMember = memberJpaRepository.findById(member.getId()).get();
+        List<Member> result1 = memberJpaRepository.findAll_Querydsl();
+        List<Member> result2 = memberJpaRepository.findByUsername_Querydsl("member1");
+
+        // then
+        assertThat(findMember).isEqualTo(member);
+        assertThat(result1).containsExactly(member);
+        assertThat(result2).containsExactly(member);
+    }
+}
