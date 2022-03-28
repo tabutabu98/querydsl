a6fcbec6d30fed15a1f978ed65638ea32a18c4d3
syc <syc@partdb.com>
2022-03-28 12:53:28 +0900

    Add: 페이징 @tabutabu98

--- src/test/java/study/querydsl/domain/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/domain/QuerydslBasicTest.java
@@ -133,4 +133,29 @@ public class QuerydslBasicTest {
         assertThat(member6.getUsername()).isEqualTo("member6");
         assertThat(memberNull.getUsername()).isNull();
     }
+
+    @Test
+    public void paging1() throws Exception {
+        List<Member> result = jpaQueryFactory.selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetch();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void paging2() throws Exception {
+        QueryResults<Member> queryResults = jpaQueryFactory.selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetchResults();
+
+        assertThat(queryResults.getTotal()).isEqualTo(4);
+        assertThat(queryResults.getLimit()).isEqualTo(2);
+        assertThat(queryResults.getOffset()).isEqualTo(1);
+        assertThat(queryResults.getResults().size()).isEqualTo(2);
+    }
 }
